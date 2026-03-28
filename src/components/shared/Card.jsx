bdcbe8526811a6a0884a18109a156c8f0ff7a058
albertumcat-boop <albert.umcat@gmail.com>
2026-03-28 13:04:53 -0400

    Create Card.jsx

---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,15 @@
+import { C } from '../../constants/colors'
+
+export default function Card({ children, onClick, style: s = {} }) {
+  return (
+    <div onClick={onClick}
+      style={{
+        background: '#fff', borderRadius: 16,
+        border: `1px solid ${C.gray200}`,
+        padding: '14px', marginBottom: 10,
+        cursor: onClick ? 'pointer' : 'default', ...s,
+      }}>
+      {children}
+    </div>
+  )
+}
