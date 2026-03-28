84c032f859ddbf8d03827ffda0487a4906a8d3b0
albertumcat-boop <albert.umcat@gmail.com>
2026-03-28 17:52:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,79 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'RutaPet',
+        short_name: 'RutaPet',
+        description: 'Gestión de ventas en ruta para proveedores de mascotas',
+        theme_color: '#0B1929',
+        background_color: '#0B1929',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        lang: 'es',
+        icons: [
+          {
+            src: 'icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+        categories: ['business', 'productivity'],
+        shortcuts: [
+          {
+            name: 'Nueva venta',
+            short_name: 'Venta',
+            description: 'Registrar una nueva venta',
+            url: '/?screen=addSale',
+            icons: [{ src: 'icons/icon-192.png', sizes: '192x192' }]
+          },
+          {
+            name: 'Ver clientes',
+            short_name: 'Clientes',
+            description: 'Ver lista de clientes',
+            url: '/?screen=clients',
+            icons: [{ src: 'icons/icon-192.png', sizes: '192x192' }]
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/tile\.openstreetmap\.org\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'osm-tiles',
+              expiration: { maxEntries: 200, maxAgeSeconds: 60 * 60 * 24 * 7 },
+            }
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'StaleWhileRevalidate',
+            options: { cacheName: 'google-fonts' }
+          },
+          {
+            urlPattern: /^https:\/\/.*\.firebaseio\.com\/.*/i,
+            handler: 'NetworkFirst',
+            options: { cacheName: 'firebase-data' }
+          }
+        ]
+      }
+    })
+  ]
 })
