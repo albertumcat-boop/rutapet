641b8d2375d64d57f44745942aa73b4e0ad5df66
albertumcat-boop <albert.umcat@gmail.com>
2026-03-28 20:21:39 -0400

    Update Card.jsx

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -2,13 +2,20 @@ import { C } from '../../constants/colors'
 
 export default function Card({ children, onClick, style: s = {} }) {
   return (
-    <div onClick={onClick}
+    <div
+      onClick={onClick}
       style={{
-        background: '#fff', borderRadius: 16,
+        background: '#fff',
+        borderRadius: 16,
         border: `1px solid ${C.gray200}`,
-        padding: '14px', marginBottom: 10,
-        cursor: onClick ? 'pointer' : 'default', ...s,
-      }}>
+        padding: '14px',
+        marginBottom: 10,
+        cursor: onClick ? 'pointer' : 'default',
+        overflow: 'hidden',
+        minWidth: 0,
+        ...s,
+      }}
+    >
       {children}
     </div>
   )
